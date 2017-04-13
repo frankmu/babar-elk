87405d1765cdc89f1a7316560acc8154bff662b6
frank <frankluck52@gmail.com>
2017-04-12 22:35:20 -0700

    change akka collector to read config from file

--- akka-stream-collector/build.sbt
+++ akka-stream-collector/build.sbt
@@ -6,9 +6,11 @@ scalaVersion := "2.11.8"
 
 val akkaVersion = "2.4.17"
 val akkaStreamKafkaVersion = "0.14"
+val configVersion = "1.3.1"
 val commonsIoVersion = "2.5"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
   "com.typesafe.akka" %% "akka-stream-kafka" % akkaStreamKafkaVersion,
+  "com.typesafe" % "config" % configVersion,
   "commons-io" % "commons-io" % commonsIoVersion)
--- akka-stream-collector/src/main/scala/com/babar/producer/PlainSinkProducerMain.scala
+++ akka-stream-collector/src/main/scala/com/babar/producer/PlainSinkProducerMain.scala
@@ -7,24 +7,23 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
+import com.typesafe.config.ConfigFactory
 import com.babar.reader.FileReader
 
-/**
-  * Created by marksu on 8/31/16.
-  */
 object PlainSinkProducerMain extends App {
 
   implicit val system = ActorSystem("PlainSinkProducerMain")
   implicit val materializer = ActorMaterializer()
+  private val config = ConfigFactory.load()
 
   val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer)
-    .withBootstrapServers("192.168.1.223:9092")
+    .withBootstrapServers(config.getString("app.kafka.host") + ":" + config.getString("app.kafka.port"))
 
-  val done = FileReader.readContinuously("/tmp/testFile.log", "UTF-8")
+  val done = FileReader.readContinuously(config.getString("app.file.path"), "UTF-8")
     .map(_.toString)
     .map { elem =>
       println(s"PlainSinkProducer produce: ${elem}")
-      new ProducerRecord[Array[Byte], String]("test", elem)
+      new ProducerRecord[Array[Byte], String](config.getString("app.kafka.topic"), elem)
     }
     .runWith(Producer.plainSink(producerSettings))
 
